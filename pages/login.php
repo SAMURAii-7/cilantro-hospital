2e72ad270df72cc04a77b2c22be5a70db54f2c7b
Shubham <shubham.prasad21@gmail.com>
2022-06-08 21:28:32 +0530

    fix login form

--- pages/login.php
+++ pages/login.php
@@ -14,15 +14,16 @@
         $pword = md5($pword);
 
         $sql = mysqli_query($con, "SELECT * FROM users WHERE username='$uName' AND password='$pword'");
-        $num = mysqli_fetch_array($sql);
-        if($num['username'] == $uName && $num['password'] == $pword) {
+        $num = mysqli_fetch_array($sql, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($sql);
+        if($count == 1) {
             session_start();
             $_SESSION['loggedIN'] = true;
             $_SESSION['name'] = $uName;
             echo "window.location.href='display.php'";
         }
         else {
-            die("Error");
+            echo "die";
         }
     }
 ?>
